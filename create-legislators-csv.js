fd4fa2f13ea98d079e1c0b082ef75d85fd759958
Chris Antaki <ChrisAntaki@gmail.com>
2016-10-22 11:10:18 -0700

    Simplifying CSV

--- create-legislators-csv.js
+++ create-legislators-csv.js
@@ -5,7 +5,7 @@ const path = require('path');
 const request = require('request-promise');
 
 
-let csv = 'title,firstname,middlename,lastname,name_suffix,nickname,party,state,district,in_office,gender,phone,fax,website,webform,congress_office,bioguide_id,votesmart_id,fec_id,govtrack_id,crp_id,twitter_id,congresspedia_url,youtube_url,facebook_id,official_rss,senate_class,birthdate,oc_email\n';
+let csv = 'title,firstname,lastname,party,state,district,in_office,phone,bioguide_id\n';
 
 Promise.resolve()
 
@@ -24,20 +24,12 @@ Promise.resolve()
         csv += [
             'Rep',
             $member.find('firstname').text(),
-            '',
             $member.find('lastname').text(),
-            '',
-            '',
             $member.find('party').text(),
             $member.find('statedistrict').text().slice(0, 2),
             +$member.find('statedistrict').text().slice(2),
             1,
-            '',
             $member.find('phone').text().replace(/[()]/g, '').replace(/ /g, '-'),
-            '',
-            '',
-            '',
-            '',
             $member.find('bioguideID').text(),
         ].join(',') + '\n';
     });
@@ -54,20 +46,12 @@ Promise.resolve()
         csv += [
             'Sen',
             $member.find('first_name').text().split(',')[0],
-            '',
             $member.find('last_name').text(),
-            '',
-            '',
             $member.find('party').text(),
             $member.find('state').text(),
             $member.find('class').text(),
             1,
-            '',
             $member.find('phone').text().replace(/[()]/g, '').replace(/ /g, '-'),
-            '',
-            '',
-            '',
-            '',
             $member.find('bioguide_id').text(),
         ].join(',') + '\n';
     });
